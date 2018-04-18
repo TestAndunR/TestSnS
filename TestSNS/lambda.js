9da6c48d73f2c4f415fd15b99cb2047103a8a256
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-04-18 15:37:11 +0530

    SNS as trigger

--- TestSNS/lambda.js
+++ TestSNS/lambda.js
@@ -1,6 +1,22 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
 	console.log("Message sent");
+	sns.publish({
+		Message: 'Testing for SNS as a resource',
+		Subject: 'TestSNS',
+		MessageAttributes: {},
+		MessageStructure: 'String',
+		TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNSTrigger'
+	}).promise()
+		.then(data => {
+			// your code goes here
+			console.log("Success");
+		})
+		.catch(err => {
+			// error handling goes here
+			console.log("Error caused");
+		});
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 }
